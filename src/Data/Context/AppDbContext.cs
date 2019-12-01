89509c83bf1da8619498a7bdaa48e13ff4fd02dc
Joshua Yates <josht.yates@btinternet.com>
2019-12-01 20:43:42 +0000

    Finish adding dbsets for models

--- src/Data/Context/AppDbContext.cs
+++ src/Data/Context/AppDbContext.cs
@@ -7,5 +7,11 @@ namespace Data.Context
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<AlbumImage> AlbumImages { get; set; }
+        public DbSet<ContactForm> ContactForms { get; set; }
+        public DbSet<CustomContent> CustomContent { get; set; }
+        public DbSet<DailySiteView> DailySiteViews { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
