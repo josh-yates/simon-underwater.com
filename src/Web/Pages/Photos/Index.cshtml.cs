f11e3194894b27ef4c6a184ec3d5d43240db7639
Joshua Yates <josht.yates@btinternet.com>
2019-12-29 22:11:46 +0000

    Add id switch for photos page

--- src/Web/Pages/Photos/Index.cshtml.cs
+++ src/Web/Pages/Photos/Index.cshtml.cs
@@ -4,6 +4,16 @@ namespace Web.Pages.Photos
 {
     public class IndexModel : BasePageModel
     {
-        
+        public bool IndividualPhoto { get; set; }
+        public string Id { get; set; }
+        public void OnGet(string id)
+        {
+            IndividualPhoto = !string.IsNullOrEmpty(id);
+
+            if (IndividualPhoto)
+            {
+                Id = id;
+            }
+        }
     }
 }
