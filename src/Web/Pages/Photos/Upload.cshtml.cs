3607eb3a131e67ddf6a8f752dadb4c1f43493026
Joshua Yates <joshua.yates@enable.com>
2020-01-08 12:54:16 +0000

    Implement basic file recognition

--- src/Web/Pages/Photos/Upload.cshtml.cs
+++ src/Web/Pages/Photos/Upload.cshtml.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Web.Pages.Shared;
 
 namespace Web.Pages.Photos
 {
     public class UploadModel : BasePageModel
     {
-        
+        [BindProperty]
+        public ICollection<IFormFile> Files { get; set; }
+
+        public IActionResult OnPostAsync()
+        {
+            return Page();
+        }
     }
 }
