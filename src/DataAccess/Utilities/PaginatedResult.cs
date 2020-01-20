d10f83ccd3aaeead5c55705a19c7f4abc43a1241
Joshua Yates <josht.yates@btinternet.com>
2020-01-20 22:00:31 +0000

    Add logic for fetching paged photos

--- src/DataAccess/Utilities/PaginatedResult.cs
+++ src/DataAccess/Utilities/PaginatedResult.cs
@@ -1,7 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DataAccess.Utilities
 {
-    public class PaginatedResult<T>
+    public class PaginatedResult<T> : List<T>
     {
-        
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedResult(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+    }
+
+    public static class PaginatedResultExtensions
+    {
+        public static async Task<PaginatedResult<T>> ToPaginatedResultAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedResult<T>(items, count, pageIndex, pageSize);
+        }
     }
 }
--- src/Web/Pages/Photos/Index.cshtml.cs
+++ src/Web/Pages/Photos/Index.cshtml.cs
@@ -1,9 +1,35 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Context;
+using Data.Models;
+using DataAccess.Utilities;
+using Microsoft.EntityFrameworkCore;
 using Web.Pages.Shared;
+using Web.Services;
 
 namespace Web.Pages.Photos
 {
     public class IndexModel : BasePageModel
     {
-        public void OnGet() {}
+        private readonly AppDbContext _dbContext;
+        public readonly ImageService _imageService;
+
+        public IndexModel(
+            AppDbContext dbContext,
+            ImageService imageService
+        )
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _imageService = imageService ?? throw new ArgumentNullException(nameof(imageService));
+        }
+        public PaginatedResult<Image> Images { get; set; }
+        public async Task OnGetAsync()
+        {
+            Images = await _dbContext
+                .Images
+                .Where(i => !i.IsDeleted)
+                .ToPaginatedResultAsync(1, 10);
+        }
     }
 }
