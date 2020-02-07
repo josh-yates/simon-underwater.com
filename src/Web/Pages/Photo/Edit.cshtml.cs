44cf07551c423edf85d71d8d65cde13ed72bbd03
Joshua Yates <joshua.yates@enable.com>
2020-02-07 13:00:55 +0000

    Begin on photo edit page

--- src/Web/Pages/Photo/Edit.cshtml.cs
+++ src/Web/Pages/Photo/Edit.cshtml.cs
@@ -1,10 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
 using Web.Pages.Shared;
 
 namespace Web.Pages.Photo
 {
     public class EditModel : BasePageModel
     {
+        [BindProperty]
+        public string Description { get; set; }
+        [BindProperty]
+        public DateTime TakenAt { get; set; }
+
         public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string TakenAtSourceMessage { get; set; }
+        
         public void OnGet(int id)
         {
             Id = id;
