056481b67d1fa67d7e500ed8fa3aae5b821c3e58
Joshua Yates <josht.yates@btinternet.com>
2020-02-20 22:45:59 +0000

    WIP on albums page

--- src/Web/Pages/Albums/Index.cshtml.cs
+++ src/Web/Pages/Albums/Index.cshtml.cs
@@ -1,9 +1,93 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Context;
+using Data.Models;
+using DataAccess.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Web.Pages.Shared;
+using Web.Utilities;
 
 namespace Web.Pages.Albums
 {
     public class IndexModel : BasePageModel
     {
-        
+        private readonly AppDbContext _dbContext;
+
+        public IndexModel(
+            AppDbContext dbContext
+        )
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        // TODO: can we unify the filters into a partial?
+        [BindProperty(SupportsGet = true)]
+        public int P { get; set; } = 1;
+
+        [BindProperty(SupportsGet = true)]
+        public string S { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime F { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime T { get; set; }
+        public PaginatedResult<Album> Albums { get; set; }
+        public DateTime? DatePickerMin { get; set; }
+        public DateTime? DatePickerMax { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            await SetFormData();
+
+            var index = P > 0 ? P : 1;
+
+            var query = _dbContext
+                .Albums
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(S))
+            {
+                var searchString = S
+                    .Trim()
+                    .ToLower();
+
+                query = query
+                    .Where(a => a.Title.Contains(searchString) ||
+                        a.Description.Contains(searchString));
+            }
+
+            if (F != default(DateTime))
+            {
+                var startData = F.StartOfDay();
+
+                
+            }
+        }
+
+        private async Task SetFormData()
+        {
+            var query = _dbContext
+                .Albums
+                .SelectMany(a => a.AlbumImages)
+                .Select(ai => ai.Image)
+                .Where(i => !i.IsDeleted)
+                .OrderBy(i => i.TakenAt)
+                .Select(i => i.TakenAt);
+
+            var min = await query.FirstOrDefaultAsync(); 
+            if (min != default(DateTimeOffset))
+            {
+                DatePickerMin = min.DateTime;
+            }
+
+            var max = await query.LastOrDefaultAsync();
+            if (max != default(DateTimeOffset))
+            {
+                DatePickerMax = max.DateTime;
+            }
+        }
     }
 }
--- src/Web/Pages/Dashboard/Index.cshtml.cs
+++ src/Web/Pages/Dashboard/Index.cshtml.cs
@@ -5,5 +5,6 @@ namespace Web.Pages.Dashboard
     public class IndexModel : BasePageModel
     {
         // TODO: Could be good to have a 'purge generated images' button here? Also maybe purge cache when we get onto caching
+        // TODO: Could also have the integration check as a one off background task too
     }
 }
