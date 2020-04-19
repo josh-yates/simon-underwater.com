ab4ff9b499437b2fccd5ca386f23c94c6eb1fe1c
Joshua Yates <josht.yates@btinternet.com>
2020-04-19 17:25:04 +0100

    WIP on contact form submission

--- src/Web/Pages/Contact/Index.cshtml.cs
+++ src/Web/Pages/Contact/Index.cshtml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Data.Context;
 using Microsoft.AspNetCore.Mvc;
@@ -17,18 +19,49 @@ namespace Web.Pages.Contact
         }
 
         [BindProperty]
-        public string UserEmail { get; set; }
+        public string Email { get; set; }
         [BindProperty]
         // Honeypot field
-        public string Email { get; set; }
+        public string AdditionalEmail { get; set; }
         [BindProperty]
         public string Name { get; set; }
         [BindProperty]
         public string Message { get; set; }
 
-        public void OnPostAsync()
+        public List<string> Errors { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
         {
-            
+            Errors = GetErrors();
+
+            if (!Errors.Any())
+            {
+
+            }
+
+            return Page();
+        }
+
+        private List<string> GetErrors()
+        {
+            var errors = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                errors.Add("Email is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(Message))
+            {
+                errors.Add("Message is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                errors.Add("Name is required");
+            }
+
+            return errors;
         }
     }
 }
