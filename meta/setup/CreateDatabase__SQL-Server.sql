b74892c61f4295ca20ad2bc0530a27878c30b47a
Joshua <josht.yates@btinternet.com>
2020-07-09 22:07:41 +0100

    Tweak setup info

--- meta/setup/CreateDatabase__SQL-Server.sql
+++ meta/setup/CreateDatabase__SQL-Server.sql
@@ -1,15 +1,4 @@
 USE master;
-GO
-CREATE LOGIN SimonUnderwaterLocal WITH PASSWORD = 'Password.123abc';    
-GO
+GO;
 CREATE DATABASE SimonUnderwaterLocal;
-GO
-
-USE [SimonUnderwaterLocal]
-GO
-CREATE USER SimonUnderwaterLocal FOR LOGIN SimonUnderwaterLocal;
-GO
-EXEC sp_addrolemember N'db_owner', N'SimonUnderwaterLocal';
-GO
-CREATE SCHEMA [SimonUnderwaterLocal] AUTHORIZATION [SimonUnderwaterLocal]
-GO
+GO;
---
+++ meta/setup/docker-setup.md
@@ -0,0 +1,7 @@
+# Pull
+
+`sudo docker pull mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04`
+
+# Run
+
+`sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=<YourStrong@Passw0rd>" \ -p 1433:1433 --name sql1 \ -d mcr.microsoft.com/mssql/server:2019-CU5-ubuntu-18.04`
